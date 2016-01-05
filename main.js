0aceca1fa681a8cd43271922cb680b7509adab17
Andrew Plotkin <zarf@Zarfslab.local>
2016-01-04 23:41:36 -0500

    Initial commit.

---
+++ main.js
@@ -0,0 +1,28 @@
+'use strict';
+const electron = require('electron');
+const app = electron.app;  // Module to control application life.
+const BrowserWindow = electron.BrowserWindow;  // Module to create native browser window.
+
+// Keep a global reference of the window object, if you don't, the window will
+// be closed automatically when the JavaScript object is garbage collected.
+let mainWindow;
+
+// Quit when all windows are closed.
+app.on('window-all-closed', function() {
+  app.quit();
+});
+
+// This method will be called when Electron has finished
+// initialization and is ready to create browser windows.
+app.on('ready', function() {
+  // Create the browser window.
+  mainWindow = new BrowserWindow({width: 800, height: 600});
+
+  // and load the index.html of the app.
+  mainWindow.loadURL('file://' + __dirname + '/play.html');
+
+  // Emitted when the window is closed.
+  mainWindow.on('closed', function() {
+    mainWindow = null;
+  });
+});
