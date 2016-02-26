8b7b037946fa09bcf496162927721b6a561df39c
Andrew Plotkin <zarf@Zarfslab.local>
2016-02-26 16:12:43 -0500

    Fix the cli script to run both locally and globally.

--- cli.sh
+++ cli.sh
@@ -1,6 +1,17 @@
 #!/bin/sh
 
-LIB="`npm root -g`"
-$LIB/lectrote/node_modules/.bin/electron $LIB/lectrote/main.js "$@" &
+# If we are in the lectrote package directory, run the app locally.
+# If not, run it from the global node directory. I think this is
+# the right way to handle this.
+
+PREFIX="`npm prefix`"
+
+if [[ "`basename $PREFIX`" != "lectrote" ]];
+then
+  LIB="`npm root -g`";
+  PREFIX="$LIB/lectrote";
+fi
+
+$PREFIX/node_modules/.bin/electron $PREFIX/main.js "$@" &
 
 
