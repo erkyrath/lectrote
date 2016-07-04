cddcf990c6859fdf054cb0d850a278408e20f6dc
Andrew Plotkin <zarf@Zarfslab.local>
2016-07-03 23:05:53 -0400

    Add ink-support files.

---
+++ inkplay.js
@@ -0,0 +1,23 @@
+'use strict';
+
+const Story = require('./ink.cjs.js').Story;
+
+function load_run(dummy, arr)
+{
+}
+
+function get_game_signature()
+{
+    return 'XXX'; //###
+}
+
+function get_metadata(key)
+{
+    return null;
+}
+
+window.GiLoad = {
+    load_run: load_run,
+    get_metadata: get_metadata,
+    get_game_signature: get_game_signature,
+};
