5e512f1cd6bc4aca9a8bd5009db35d2ea2d6f6af
Andrew Plotkin <zarf@Zarfslab.local>
2016-10-01 19:00:12 -0400

    Push the story buffer into Parchment.

--- zplay.js
+++ zplay.js
@@ -1,10 +1,20 @@
 'use strict';
 
+var all_options = {
+};
+
 /* Launch the game. The buf argument must be a Node Buffer.
  */
 function load_run(optobj, buf)
 {
-    parchment.load_library();
+    if (!optobj)
+        optobj = window.game_options;
+    if (optobj)
+        jQuery.extend(all_options, optobj);
+
+    all_options.default_story = [ buf ];
+
+    parchment.load_library(all_options);
 }
 
 function get_metadata(key)
